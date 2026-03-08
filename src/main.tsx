2ac2e5f21a01c677c59a1c1b896bd96530003e76
zesciati <zakyabiyyu2003@gmail.com>
2026-03-08 22:25:36 +0700

    login.tsx -> LoginPage.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import './index.css'
 // import App from './App.tsx'
-import Login from './login.tsx'
+import Login from './LoginPage.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
