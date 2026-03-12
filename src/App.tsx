d7e9fbecbd8007f3024aa55b0d7eb9bdfffd8b2f
zesciati <zakyabiyyu2003@gmail.com>
2026-03-12 13:09:31 +0700

    fixing

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
-import { Routes, Route, Navigate } from 'react-router-dom'
-import LoginPage from './LoginPage.tsx'
-import MenuCustomerPage from './MenuCustomerPage.tsx'
-import CustomerPackagePurchase from './CustomerPackagePurchasePage.tsx'
-
+import { Routes, Route, Navigate } from "react-router-dom";
+import LoginPage from "./LoginPage.tsx";
+import MenuCustomerPage from "./MenuCustomerPage.tsx";
+import CustomerPackagePurchase from "./CustomerPackagePurchasePage.tsx";
 const App = () => {
   return (
     <Routes>
-      {/* Redirect dari "/" ke "/login" */}
+      {}
       <Route path="/" element={<Navigate to="/login" replace />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/menucustomer" element={<MenuCustomerPage />} />
-      <Route path="/customerpackagepurchase" element={<CustomerPackagePurchase />} />
+      <Route
+        path="/customerpackagepurchase"
+        element={<CustomerPackagePurchase />}
+      />
     </Routes>
-  )
-}
-
-export default App
+  );
+};
+export default App;
